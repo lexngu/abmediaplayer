f9e6aa559943b4bc9870287d4f110deac9a60aa9
Alexander Nguyen <alexander.nguyen@stud.hfm-karlsruhe.de>
2023-03-11 17:16:26 +0100

    Initial Commit

---
+++ ABMediaPlayer/ABMediaPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ABMediaPlayerApp.swift
+//  ABMediaPlayer
+//
+//  Created by Alexander Nguyen on 11.03.23.
+//
+
+import SwiftUI
+
+@main
+struct ABMediaPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
